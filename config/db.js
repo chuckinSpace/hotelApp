ca51a08aaf4f08eb4064913bbe69b900b8c90364
Carlos Moyano <carlosmoyanor@gmail.com>
2019-07-20 21:32:07 -0400

    added mongoDB and mention

--- config/db.js
+++ config/db.js
@@ -2,5 +2,6 @@
 // To run locally use 'mongodb://localhost/mern-crud' instead of process.env.DB
 
 module.exports = {
-  db: process.env.DB
+  db :'mongodb://carlosmoyanor:mydatabase2@ds353457.mlab.com:53457/hotelapp',
+  secretOrKey: 'secret'
 };
